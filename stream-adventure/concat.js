0ead3a599383ff0828ca95581bac3d47ad75cf2c
Yao Hao <yhaoao@163.com>
2014-09-06 11:13:54 +0800

    add concat

--- stream-adventure/concat.js
+++ stream-adventure/concat.js
@@ -1,2 +1,8 @@
  var concat = require('concat-stream');
- var http = require('http');
+
+ process.stdin
+	.pipe(concat(function(temp){
+		temp=temp||'';
+		var result=temp.toString().split('').reverse().join('');
+		process.stdout.write(result);
+	}));
